634b8989a2540878c4db9a4b518d2290885f7e4a
Stefan Dörig <doerig@cyberlink.ch>
2014-05-09 16:32:49 +0200

    Some restructuring

---
+++ testrest/main.py
@@ -0,0 +1,13 @@
+from testrest.asserter import AssertTrue
+
+__author__ = 'doerig'
+
+
+
+def main():
+    a = AssertTrue.AssertTrue()
+    print(a.isSuccess())
+
+
+if __name__ == "__main__":
+    main()
