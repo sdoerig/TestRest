1b020520a17ab35097f2042285f6a60ac3a98adc
Stefan Dörig <doerig@cyberlink.ch>
2014-05-11 10:00:37 +0200

    Renamed

---
+++ src/TestRest.py
@@ -0,0 +1,35 @@
+import sys, getopt
+
+from testrest.asserter import AssertTrue
+
+__author__ = 'doerig'
+
+
+
+def main():
+    execName = sys.argv[0]
+    argv = sys.argv[1:]
+    configfile = None
+    
+    try:
+        opts, args = getopt.getopt(argv,"hc:",["config="])
+    except getopt.GetoptError:
+        print (execName + " -c <inputfile> ")
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print (execName + ' -c <inputfile>')
+            sys.exit()
+        elif opt in ("-c", "--config"):
+            configfile = arg
+    if configfile == None:
+        print ("No configFile given. Usage: " + execName + " -c <inputfile> ")
+        sys.exit(2)
+    print ('Config file is "' + configfile)
+    a = AssertTrue.AssertTrue()
+    a.doAssert()
+    print(a.isSuccess())
+
+
+if __name__ == "__main__":
+    main()
