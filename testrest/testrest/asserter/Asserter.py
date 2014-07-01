03616a64797dc9106d2c9fd8cb8c05872fa85ccc
Stefan Dörig <sdoerig@bluewin.ch>
2014-07-01 18:08:03 +0200

    Merged Asserters

---
+++ testrest/testrest/asserter/Asserter.py
@@ -0,0 +1,76 @@
+from testrest.asserter.AbstractAssert import AbstractAssert
+'''
+Created on Jun 30, 2014
+
+@author: doerig
+'''
+
+class AssertIsGreater(AbstractAssert):
+    '''
+    classdocs
+    '''
+    logger = None
+    _a = None
+
+    def __init__(self, a):
+        '''
+        Constructor
+        '''
+        super().__init__()
+        self._a = a
+        AssertIsGreater.logger.debug("AssertIsGreater: a " + str(self._a))
+
+    def doAssert(self, expr, msg):
+        AssertIsGreater.logger.debug("doAssert called...")
+        try:
+            self.assertGreater(self._a, expr, msg)
+            self.setSuccess(True)
+        except AssertionError as error:
+            AssertIsGreater.logger.error("Assertion failed: " + str(error))
+            self.setSuccess(False)
+        except Exception as error:
+            AssertIsGreater.logger.error("Assertion failed: " + str(error))
+            self.setSuccess(False)
+            
+            
+class AssertIsNotNone(AbstractAssert):
+    '''
+    classdocs
+    '''
+    logger = None
+
+    def __init__(self, *argv, **kv):
+        '''
+        Constructor
+        '''
+        super().__init__()
+
+    def doAssert(self, expr, msg):
+        AssertIsNotNone.logger.debug("doAssert called...")
+        try:
+            self.assertIsNotNone(expr, msg)
+            self.setSuccess(True)
+        except AssertionError as error:
+            AssertIsNotNone.logger.error("Assertion failed: " + str(error))
+            self.setSuccess(False)
+            
+class AssertTrue(AbstractAssert):
+    '''
+    classdocs
+    '''
+    logger = None
+
+    def __init__(self, *argv, **kv):
+        '''
+        Constructor
+        '''
+        super().__init__()
+
+    def doAssert(self, expr, msg):
+        AssertTrue.logger.debug("doAssert called...")
+        try:
+            self.assertTrue(expr, msg)
+            self.setSuccess(True)
+        except AssertionError as error:
+            AssertTrue.logger.error("Assertion failed: " + str(error))
+            self.setSuccess(False)
